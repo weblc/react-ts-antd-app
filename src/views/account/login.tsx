84d781656c1eeb1d3e4ff8e4a8ecdd4a52eca896
laoC <webl_c@163.com>
2020-03-31 23:47:19 +0800

    11

--- src/views/account/login.tsx
+++ src/views/account/login.tsx
@@ -53,7 +53,7 @@ class Login extends Component<any> {
 
                 <Form.Item>
                     <Button type="primary" htmlType="submit" className="login-form-button">
-                        登陆
+                        登陆11
                     </Button>
                 </Form.Item>
             </Form>
