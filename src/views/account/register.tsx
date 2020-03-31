00f263cdf80dc5c9d5e6f5450063d3ec3a9cd76f
czy <webl_c@163.com>
2020-03-31 19:44:22 +0800

    123

--- src/views/account/register.tsx
+++ src/views/account/register.tsx
@@ -59,7 +59,7 @@ import { Form,  Input, Button  } from "antd";
                         htmlType="submit"
                         className="login-form-button"
                     >
-                        登录
+                        登录122
                     </Button>
                 </Form.Item>
             </Form>
