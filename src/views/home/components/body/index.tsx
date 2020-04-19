9ba7728655d2bcd52542570a6268027794785ad1
czy <webl_c@163.com>
2020-04-19 23:54:43 +0800

    首页tabs

--- src/views/home/components/body/index.tsx
+++ src/views/home/components/body/index.tsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 
-
-import { Row, Col } from "antd";
+import { Row, Tabs, Col } from "antd";
 import { Movies } from "@/components";
 import Nav from "./nav";
 import styles from "./body.module.less";
-
-
+import { AppleOutlined, AndroidOutlined } from "@ant-design/icons";
+const { TabPane } = Tabs;
 interface movie {
     title: string;
     description: string;
@@ -18,116 +17,125 @@ interface movie {
 const ItemList: movie[] = [
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 4.5,
-        hot: 2.5
+        hot: 2.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 3.5
+        hot: 3.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 4.5
+        hot: 4.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 1.5
+        hot: 1.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 0.5
+        hot: 0.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 2.5
+        hot: 2.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 0.5
+        hot: 0.5,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 0
+        hot: 0,
     },
     {
         title: "敏狗的故事",
-        description:
-            "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
-        poster:
-            "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
+        description: "70多岁独居老人在偏僻贫困的村庄与一条哈士奇的奇闻趣事.....",
+        poster: "https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png",
         score: 7.9,
-        hot: 5
-    }
+        hot: 5,
+    },
 ];
+
+const navList: Array<string> = ["亚洲", "欧美", "韩国", "日本", "泰国", "印度"];
 
 export default class Body extends Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
-            counts: 10
+            counts: 10,
         };
     }
 
     componentDidMount(): void {
         // getTest().then((res)=>{
-
         // });
-
     }
     test = (item: any): void => {
-
-       this.props.history.push({
-            pathname: '/movie/details',
+        this.props.history.push({
+            pathname: "/movie/details",
         });
-    }
-    componentWillUnmount(): void { }
+    };
+    componentWillUnmount(): void {}
     render() {
-
         return (
             <Row className={styles.body_wrapper}>
                 <Col>
+                    <Tabs defaultActiveKey="0" className={styles.nav_style}>
+                        {navList.map((item, index: number) => {
+                            return (
+                                <TabPane
+                                    tab={
+                                        <span>
+                                            <AppleOutlined />
+                                            {item}
+                                        </span>
+                                    }
+                                    key={`${index}`}
+                                >
+                                    <Row>
+                                        {ItemList.map((item, index) => {
+                                            return (
+                                                <Col lg={4} sm={6} key={index}>
+                                                    <Row justify="center" onClick={this.test.bind(this, item)}>
+                                                        <Movies {...item} />
+                                                    </Row>
+                                                </Col>
+                                            );
+                                        })}
+                                    </Row>
+                                </TabPane>
+                            );
+                        })}
+                    </Tabs>
+                </Col>
+                {/* <Col>
                     <Nav />
                 </Col>
-                {/* <Row type="flex">{ItemList(this.state.counts)}</Row> */}
+              
                 <Row >
                     {ItemList.map((item, index) => {
                         return (
@@ -140,7 +148,7 @@ export default class Body extends Component<any, any> {
                             </Col>
                         );
                     })}
-                </Row>
+                </Row> */}
             </Row>
         );
     }
