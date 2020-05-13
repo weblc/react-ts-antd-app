90563372d4d2ba1239831bac6057c36159f4d824
czy <webl_c@163.com>
2020-05-13 18:35:47 +0800

    bug fix

--- src/views/manage/task/index.tsx
+++ src/views/manage/task/index.tsx
@@ -2,10 +2,12 @@
 
 import React, { Component } from "react";
 
-import { Table, Tag, Pagination, Row } from "antd";
+import { Table, Tag, Pagination, message } from "antd";
 
 import { SpaceRow } from "@/components";
 import From from "./form";
+
+import {mangerApi} from '@/api'
 
 import classnames from "classnames";
 import "./index.less";
@@ -13,40 +15,53 @@ import "./index.less";
 const columns = [
     {
         title: "任务id",
-        dataIndex: "name",
-        key: "name",
+        dataIndex: "id",
+        key: "id",
         render: (text: any) => <a>{text}</a>,
     },
     {
-        title: "任务日期",
-        dataIndex: "age",
-        key: "age",
+        title: "任务名",
+        dataIndex: "title",
+        key: "title",
     },
     {
-        title: "状态",
-        dataIndex: "address",
-        key: "address",
+        width:200,
+        title: "任务详情",
+        dataIndex: "content",
+        key: "content",
     },
     {
-        title: "标签",
-        key: "tags",
-        dataIndex: "tags",
-        render: (tags: any) => (
-            <span>
-                {tags.map((tag: any) => {
-                    let color = tag.length > 5 ? "geekblue" : "green";
-                    if (tag === "loser") {
-                        color = "volcano";
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                             {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </span>
-        ),
+
+        title: "开始时间",
+        dataIndex: "startTime",
+        key: "startTime",
     },
+    {
+
+        title: "结束时间",
+        dataIndex: "endTime",
+        key: "endTime",
+    },
+    // {
+    //     title: "标签",
+    //     key: "tags",
+    //     dataIndex: "tags",
+    //     render: (tags: any) => (
+    //         <span>
+    //             {tags.map((tag: any) => {
+    //                 let color = tag.length > 5 ? "geekblue" : "green";
+    //                 if (tag === "loser") {
+    //                     color = "volcano";
+    //                 }
+    //                 return (
+    //                     <Tag color={color} key={tag}>
+    //                         {tag.toUpperCase()}
+    //                     </Tag>
+    //                 );
+    //             })}
+    //         </span>
+    //     ),
+    // },
     {
         title: "操作",
         key: "action",
@@ -58,42 +73,33 @@ const columns = [
         ),
     },
 ];
-const data = [
-    {
-        key: "1",
-        name: "John Brown",
-        age: 32,
-        address: "New York No. 1 Lake Park",
-        tags: ["普通任务"],
-    },
-    {
-        key: "2",
-        name: "Jim Green",
-        age: 42,
-        address: "London No. 1 Lake Park",
-        tags: ["观影"],
-    },
-    {
-        key: "3",
-        name: "Joe Black",
-        age: 32,
-        address: "Sidney No. 1 Lake Park",
-        tags: ["cool", "teacher"],
-    },
-];
+
 class Task extends Component<any, any> {
     constructor(props: any) {
         super(props);
         this.state = {
+            tableData:[],
             top: "topLeft",
             bottom: "bottomRight",
         };
     }
+    searchHandle = (val:any)=>{
+        mangerApi.getUserTaskList(val).then(({success,message,data})=>{
+            if(success){
+                console.log(data)
+                this.setState({
+                    tableData:data
+                })
+            }else{
+                message.error(message);
+            }
+        })
+    }
     render() {
         return (
             <div className={classnames("manage-Layout")}>
-                <From></From>
-                <Table columns={columns} dataSource={data} pagination={false} />
+                <From searchHandle = {this.searchHandle}></From>
+                <Table columns={columns} dataSource={this.state.tableData} pagination={false} rowKey={(item,index)=>item.id}/>
                 <SpaceRow>
                     <Pagination defaultCurrent={0} total={20} />
                 </SpaceRow>
